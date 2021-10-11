be765b1458051728e252ea322b5c4485467dbf9c
dmurillo976s <diego.murillo@spartanapproach.com>
2021-10-11 00:05:28 -0600

    added static documentation

--- api/modules/data_classes.py
+++ api/modules/data_classes.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, Tuple
 
 # Pydantic data classes definition------------------------------------
@@ -6,9 +6,19 @@ from typing import Optional, Tuple
 
 # Data class for User response verification
 class BaseUser(BaseModel):
-    id: str
-    name: str
-    email: str
+    id: str = Field(..., description="Unique value for identifying a user")
+    name: str = Field(..., description="Common user name")
+    email: str = Field(..., description="Email of the user")
+
+    class Config:
+        schema_extra = {
+            "description": "Data model used for responses that carry user information",
+            "example": {
+                "id": "myUserID01",
+                "name": "John Doe",
+                "email": "jd@gmail.com"
+            }
+        }
 
 
 def init_BaseUser(values: Tuple[str, str, str]):
@@ -17,21 +27,50 @@ def init_BaseUser(values: Tuple[str, str, str]):
 
 # Data class for new User request verification
 class InUser(BaseUser):
-    password: str
+    password: str = Field(..., description="Password string of the user")
+
+    schema_extra = {
+        "description": "Data model used for input of new user data",
+        "example": {
+            "id": "myUserID01",
+            "name": "John Doe",
+            "email": "jd@gmail.com",
+            "password": "123abc"
+        }
+    }
 
 
 # Data class for User update request verification
 class UpdateUser(BaseModel):
-    name: Optional[str] = None
-    email: Optional[str] = None
-    password: Optional[str] = None
+    name: Optional[str] = Field(None, description="Optional updated name value")
+    email: Optional[str] = Field(None, description="Optional updated email value")
+    password: Optional[str] = Field(None, description="Optional updated password value")
+
+    schema_extra = {
+        "description": "Data model used for input of updated user data",
+        "example": {
+            "name": "John Doe",
+            "email": "jd@gmail.com",
+            "password": "123abc"
+        }
+    }
 
 
 # Data class for Team response and new Team request verification
 class BaseTeam(BaseModel):
-    id: str
-    name: str
-    description: str
+    id: str = Field(..., description="Unique value for identifying a team")
+    name: str = Field(..., description="Unique name of the team")
+    description: str = Field(..., description="Simple description of the team")
+
+    class Config:
+        schema_extra = {
+            "description": "Data model used for responses that carry team information",
+            "example": {
+                "id": "myTeamID01",
+                "name": "Legends",
+                "description": "Very efficient team"
+            }
+        }
 
 
 def init_BaseTeam(values: Tuple[str, str, str]):
@@ -40,5 +79,14 @@ def init_BaseTeam(values: Tuple[str, str, str]):
 
 # Data class for Team update request verification
 class UpdateTeam(BaseModel):
-    name: Optional[str] = None
-    description: Optional[str] = None
+    name: Optional[str] = Field(None, description="Optional updated name value")
+    description: Optional[str] = Field(None, description="Optional updated description value")
+
+    class Config:
+        schema_extra = {
+            "description": "Data model used for input of updated team information",
+            "example": {
+                "name": "Legends",
+                "description": "Very efficient team"
+            }
+        }
